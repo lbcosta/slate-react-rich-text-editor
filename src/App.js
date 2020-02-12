2bfec404223b3fd5b35ead7de02402a5e060f297
Leonardo Costa <lbcosta.dev@gmail.com>
2020-02-12 20:58:00 -0300

    Slate Walkthrough - Defining Custom Elements

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React, { useMemo, useState } from "react";
+import React, { useMemo, useState, useCallback } from "react";
 
 // Import the Slate editor factory.
-import { createEditor } from "slate";
+import { createEditor, Transforms, Editor } from "slate";
 
 // Import the Slate components and React plugin.
 import { Slate, Editable, withReact } from "slate-react";
+
+const CodeElement = props => {
+  return (
+    <pre {...props.attributes}>
+      <code>{props.children}</code>
+    </pre>
+  );
+};
+
+const DefaultElement = props => {
+  return <p {...props.attributes}>{props.children}</p>;
+};
 
 function App() {
   // Create a Slate editor object that wont't change across renders.
@@ -18,18 +30,38 @@ function App() {
     }
   ]);
 
+  // Define a rendering function based on the element passed to `props`. We use
+  // `useCallback` here to memoize the function for subsequent renders.
+  const renderElement = useCallback(props => {
+    switch (props.element.type) {
+      case "code":
+        return <CodeElement {...props} />;
+      default:
+        return <DefaultElement {...props} />;
+    }
+  }, []);
+
   // Render the Slate context.
   return (
     <Slate editor={editor} value={value} onChange={v => setValue(v)}>
       {/* Add the editable component inside the context. */}
-      { /* Define a new handler which prints the key that was pressed. */ }
-      <Editable 
+      {/* Define a new handler which prints the key that was pressed. */}
+      {/* Pass in the 'renderElement' function */}
+      <Editable
+        renderElement={renderElement}
         onKeyDown={event => {
-          if (event.key === '&') {
-            // Prevent the ampersand character from being inserted.
-            event.preventDefault()
-            // Execute the `insertText` method when the event occurs.
-            editor.insertText("and")
+          if (event.key === ";" && event.ctrlKey) {
+            event.preventDefault();
+            // Determine whether any of the currently selected blocks are code blocks.
+            const [match] = Editor.nodes(editor, {
+              match: n => n.type === "code"
+            });
+            // Toggle the block type depending on whether there's already a match.
+            Transforms.setNodes(
+              editor,
+              { type: match ? "paragraph" : "code" },
+              { match: n => Editor.isBlock(editor, n) }
+            );
           }
         }}
       />
